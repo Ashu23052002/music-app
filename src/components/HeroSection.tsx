4c16a46d60ae2b261ef5497afd1c2de3b3276dcc
Ashu23052002 <ans23052002@gmail.com>
2024-06-28 14:02:57 +0530

    c

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
- import { Button } from "./ui/moving-border";
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
@@ -12,18 +12,18 @@ function HeroSection() {
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you're a beginner or looking to refine your
+          journey today. Whether you are a beginner or looking to refine your
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
           <Link href={"/courses"}>
-                    <Button
-                    borderRadius="1.75rem"
-                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                    >
-                    Explore courses
-                    </Button>
-                </Link>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore courses
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
