4a21dcca7f834473018dcf6677ba68755b284c5e
Ashu23052002 <ans23052002@gmail.com>
2024-06-28 08:52:57 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <p>Ashu</p>
      <HeroSection />
      <FeaturedCourses />
      <WhyChooseUs />
